5b1ac7586dfecc346b80f981f1d44f49d7609aa1
AlejandroPerdomoHP <alejandro.perdomo.cuello@outlook.com>
2020-08-17 16:48:48 +0200

    I dont know what I have changed, buts its not good

--- script.js
+++ script.js
@@ -10,31 +10,32 @@ Information on getting the API to work
 
 // Now to break the weblink to add the city name
 
-const weatherNow1 = "https://api.openweathermap.org/data/2.5/weather?q=";
+const weatherToday1 = "https://api.openweathermap.org/data/2.5/weather?q=";
 
-const weatherNow2 = "&units=metric&appid=50b9caeb9f022d5e06aded241bfda347";
+const weatherToday2 = "&units=metric&appid=50b9caeb9f022d5e06aded241bfda347";
 
 // now to break up the weblink to add the city name I THNK I AWS SUPPOSE TO USE THIS ONE ALL THE TIME
 
-const weather51 = "https://api.openweathermap.org/data/2.5/forecast?q=";
+const weatherFuture1 = "https://api.openweathermap.org/data/2.5/forecast?q=";
 
-const weather52 = "&units=metric&appid=50b9caeb9f022d5e06aded241bfda347";
+const weatherFuture2 = "&units=metric&appid=50b9caeb9f022d5e06aded241bfda347";
+
+// All elements below are for the First Button
 
 // Function for the Button press that gets the City name from the user
-document.getElementById("mainBtn")
-    .addEventListener("click", runTwoFunctions1);
+document.getElementById("button1").addEventListener("click", runTwoFunctions1);
 
 function getCityName() {
     let city = document.getElementById("cityName1").value;
 
     // Here we construct the weblink with the proper name
-    let urlNow = weatherNow1 + city + weatherNow2;
+    let url = weatherToday1 + city + weatherToday2;
 
     // this is new, so I dont know what I am doing
 
-    console.log(fetch(urlNow));
+    console.log(fetch(url));
 
-    fetch(urlNow)
+    fetch(url)
         .then((response) => {
             if (response.ok) {
                 console.log("Success")
@@ -46,8 +47,8 @@ function getCityName() {
         })
         .then((data) => {
             // Work with JSON data here
-            let numberNow = data.main.temp + "°C";
-            document.getElementById("mainOutput").innerHTML = numberNow;
+            let weatherTodayCity1 = data.main.temp + "°C";
+            document.getElementById("output1").innerHTML = weatherTodayCity1;
         })
         .catch((err) => {
             // Do something for an error here
@@ -55,19 +56,19 @@ function getCityName() {
         });
 };
 
-// Making the function to get the Temp of the next 5 days
+// Making the function to get the Temp of the next 5 days of City 1
 function getFutureTemp() {
-    let city5 = document.getElementById("cityName1").value;
+    let city1 = document.getElementById("cityName1").value;
 
     // Here we construct the weblink with the proper name
-    let url5 = weather51 + city5 + weather52;
-    console.log(url5);
+    let url1 = weatherFuture1 + city1 + weatherFuture2;
+    console.log(url1);
 
     // this is new, so I dont know what I am doing
 
-    console.log(fetch(url5));
+    console.log(fetch(url1));
 
-    fetch(url5)
+    fetch(url1)
         .then((response) => {
             if (response.ok) {
                 console.log("Success")
@@ -77,9 +78,9 @@ function getFutureTemp() {
 
             return response.json();
         })
-        .then((data5) => {
+        .then((data1) => {
             // Work with JSON data here
-            workWithData(data5);
+            workWithData(data1);
         })
         .catch((err) => {
             // Do something for an error here
@@ -88,23 +89,47 @@ function getFutureTemp() {
 
 };
 
+// Function to wor with the table data for city 1
+function workWithData(data1) {
+    console.log("Now processing data1");
+
+    document.getElementById("output11").innerHTML += "The weather for " + data1.city.name + ", " + data1.city.country + " is as follows:";
+    document.getElementById("output11").innerHTML += "<br>";
+    document.getElementById("output11").innerHTML += data1.list[0].dt_txt + ": " + data1.list[0].main.temp + "°C";
+    document.getElementById("output11").innerHTML += "<br>";
+    document.getElementById("output11").innerHTML += data1.list[8].dt_txt + ": " + data1.list[8].main.temp + "°C";
+    document.getElementById("output11").innerHTML += "<br>";
+    document.getElementById("output11").innerHTML += data1.list[16].dt_txt + ": " + data1.list[16].main.temp + "°C";
+    document.getElementById("output11").innerHTML += "<br>";
+    document.getElementById("output11").innerHTML += data1.list[24].dt_txt + ": " + data1.list[24].main.temp + "°C";
+    document.getElementById("output11").innerHTML += "<br>";
+    document.getElementById("output11").innerHTML += data1.list[32].dt_txt + ": " + data1.list[32].main.temp + "°C";
+    document.getElementById("output11").innerHTML += "<br>";
+    document.getElementById("output11").innerHTML += data1.list[40].dt_txt + ": " + data1.list[40].main.temp + "°C";
+};
+
 // a function to run two functions at the same time
 
-function runTwoFunctions1 () {
+function runTwoFunctions1() {
     getCityName();
     getFutureTemp();
 };
 
-// Function for the Button2 press that gets the City name from the user
-document.getElementById("btn2")
-    .addEventListener("click", getCityName2);
 
+
+// All elements below are for the Second Button
+
+// Function for the Button2 press that gets the City name from the user
+document.getElementById("button2")
+    .addEventListener("click", runTwoFunctions2);
+
+// Function to get City name 2 for the second city
 function getCityName2() {
     let city2 = document.getElementById("cityName2").value;
 
     // Here we construct the weblink with the proper name
 
-    let url2 = weatherNow1 + city2 + weatherNow2;
+    let url2 = weatherFuture1 + city2 + weatherFuture2;
 
     // this is new, so I dont know what I am doing
 
@@ -122,8 +147,8 @@ function getCityName2() {
         })
         .then((data2) => {
             // Work with JSON data here
-            let number2 = data2.main.temp + "°C";
-            document.getElementById("secondOutput").innerHTML = number2;
+            let WeatherTodayCity2 = data.main.temp + "°C";
+            document.getElementById("output2").innerHTML = WeatherTodayCity2;
         })
         .catch((err) => {
             // Do something for an error here
@@ -131,23 +156,61 @@ function getCityName2() {
         });
 };
 
-// Function to wor with the table data 
-function workWithData(data5) {
-    console.log("Now processing data5");
+function getFutureTemp2() {
+    let city3 = document.getElementById("cityName1").value;
 
-    document.getElementById("output5").innerHTML += "The weather for " + data5.city.name + ", " + data5.city.country + " is as folloms:";
-    document.getElementById("output5").innerHTML += "<br>";
-    document.getElementById("output5").innerHTML += data5.list[0].dt_txt + ": " + data5.list[0].main.temp + "°C";
-    document.getElementById("output5").innerHTML += "<br>";
-    document.getElementById("output5").innerHTML += data5.list[8].dt_txt + ": " + data5.list[8].main.temp + "°C";
-    document.getElementById("output5").innerHTML += "<br>";
-    document.getElementById("output5").innerHTML += data5.list[16].dt_txt + ": " + data5.list[16].main.temp + "°C";
-    document.getElementById("output5").innerHTML += "<br>";
-    document.getElementById("output5").innerHTML += data5.list[24].dt_txt + ": " + data5.list[24].main.temp + "°C";
-    document.getElementById("output5").innerHTML += "<br>";
-    document.getElementById("output5").innerHTML += data5.list[32].dt_txt + ": " + data5.list[32].main.temp + "°C";
-    document.getElementById("output5").innerHTML += "<br>";
-    document.getElementById("output5").innerHTML += data5.list[40].dt_txt + ": " + data5.list[40].main.temp + "°C";
+    // Here we construct the weblink with the proper name
+    let url3 = weatherFuture1 + city3 + weatherFuture2;
+    console.log(url3);
+
+    // this is new, so I dont know what I am doing
+
+    console.log(fetch(url3));
+
+    fetch(url3)
+        .then((response) => {
+            if (response.ok) {
+                console.log("Success")
+            } else {
+                console.log("Error")
+            }
+            return response.json();
+        })
+        .then((data2) => {
+            // Work with JSON data here
+            workWithData(data2);
+        })
+        .catch((err) => {
+            // Do something for an error here
+            console.log("There was an Error getting the 5 days ahead Data")
+        }, 3000); // the 3000 is the amounts of miliseconds to wait until the data is here, in this case 3 seconds
+
+};
+
+// Function to wor with the table data for city 2
+function workWithData2(data2) {
+    console.log("Now processing data2");
+
+    document.getElementById("output22").innerHTML += "The weather for " + data2.city.name + ", " + data2.city.country + " is as follows:";
+    document.getElementById("output22").innerHTML += "<br>";
+    document.getElementById("output22").innerHTML += data2.list[0].dt_txt + ": " + data2.list[0].main.temp + "°C";
+    document.getElementById("output22").innerHTML += "<br>";
+    document.getElementById("output22").innerHTML += data2.list[8].dt_txt + ": " + data2.list[8].main.temp + "°C";
+    document.getElementById("output22").innerHTML += "<br>";
+    document.getElementById("output22").innerHTML += data2.list[16].dt_txt + ": " + data2.list[16].main.temp + "°C";
+    document.getElementById("output22").innerHTML += "<br>";
+    document.getElementById("output22").innerHTML += data2.list[24].dt_txt + ": " + data2.list[24].main.temp + "°C";
+    document.getElementById("output22").innerHTML += "<br>";
+    document.getElementById("output22").innerHTML += data2.list[32].dt_txt + ": " + data2.list[32].main.temp + "°C";
+    document.getElementById("output22").innerHTML += "<br>";
+    document.getElementById("output22").innerHTML += data2.list[40].dt_txt + ": " + data2.list[40].main.temp + "°C";
+};
+
+// a function to run two functions at the same time
+
+function runTwoFunctions2() {
+    getCityName2();
+    getFutureTemp2();
 };
 
 // Function for the Enter  Key for the cityName1 input
@@ -155,7 +218,7 @@ let input = document.getElementById("cityName1");
 input.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
-        document.getElementById("mainBtn").click();
+        document.getElementById("button1").click();
     }
 });
 
@@ -164,7 +227,7 @@ let input2 = document.getElementById("cityName2");
 input2.addEventListener("keyup", function (event) {
     if (event.keyCode === 13) {
         event.preventDefault();
-        document.getElementById("btn2").click();
+        document.getElementById("button2").click();
     }
 });
 
